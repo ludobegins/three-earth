dea164687c723493cbf8f6e506c3b4252f790779
ludobegins <ludo.beghin@gmail.com>
2022-12-17 18:30:06 -0300

    add location and rotate along earth

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 
 const canvasContainer = document.querySelector('#canvasContainer')
 
+const EARTH_RADIUS = 6
+const EARTH_ROTATION = 0.002
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.1, 1000 )
 
@@ -26,7 +29,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 // sphere
 
 const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(6, 50, 50),
+  new THREE.SphereGeometry(EARTH_RADIUS, 50, 50),
   // new THREE.MeshBasicMaterial({
   //   map: new THREE.TextureLoader().load('img/earth_uv.jpg')
   // })
@@ -44,7 +47,7 @@ const sphere = new THREE.Mesh(
 // atmosphere
 
 const atmosphere = new THREE.Mesh(
-  new THREE.SphereGeometry(6.6, 50, 50),
+  new THREE.SphereGeometry(EARTH_RADIUS * 1.1, 50, 50),
   new THREE.ShaderMaterial({
     vertexShader: atmosphereVertexShader,
     fragmentShader: atmosphereFragmentShader,
@@ -80,6 +83,33 @@ starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVerti
 const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 
+// locations
+
+const locGeometry = new THREE.SphereBufferGeometry(0.08, 24, 24)
+const locMaterial = new THREE.MeshStandardMaterial({color: 0xFF0000})
+
+const loc = new THREE.Mesh( locGeometry, locMaterial )
+
+let lat = 18
+let lon = -2
+
+lat = ( lat ) * Math.PI / 180
+lon = ( lon ) * Math.PI / 180 + Math.PI / 2
+
+const locX = Math.cos(lat) * Math.sin(lon) * EARTH_RADIUS
+const locY = Math.sin(lat) * EARTH_RADIUS
+const locZ = Math.cos(lat) * Math.cos(lon) * EARTH_RADIUS
+
+console.log('x,y,z:', locX, locY, locZ)
+
+loc.position.set(locX, locY, locZ)
+scene.add(loc)
+
+// ambient light
+
+const light = new THREE.AmbientLight( 0x404040 );
+scene.add( light );
+
 camera.position.z = 15
 
 const mouse = {
@@ -92,7 +122,15 @@ const controls = new OrbitControls(camera, canvasContainer)
 function animate() {
   requestAnimationFrame( animate )
   renderer.render(scene, camera)
-  sphere.rotation.y += 0.002
+
+  sphere.rotation.y += EARTH_ROTATION
+
+  loc.position.set(
+    locX * Math.cos(sphere.rotation.y) + locZ * Math.sin(sphere.rotation.y),
+    locY,
+    - locX * Math.sin(sphere.rotation.y) + locZ * Math.cos(sphere.rotation.y)
+  )
+
   controls.update()
 }
 
