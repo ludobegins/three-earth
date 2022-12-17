982ad77fa84949a3afdad451d6d6eb07afdbd2a9
ludobegins <ludo.beghin@gmail.com>
2022-12-17 14:54:12 -0300

    add atmosphere

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import * as THREE from 'three'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 )
 
@@ -38,6 +41,20 @@ const sphere = new THREE.Mesh(
 )
 
 scene.add(sphere)
+
+// atmosphere
+
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5.5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+)
+
+scene.add(atmosphere)
 
 camera.position.z = 15
 
