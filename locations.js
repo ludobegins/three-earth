69b802563511bd993f4348627b3c2bfac0771524
ludobegins <ludo.beghin@gmail.com>
2023-04-06 19:52:35 -0300

    add 3 lugares + html

--- locations.js
+++ locations.js
@@ -11,6 +11,27 @@ export const locations = [
         lat: -19,
         lon: 30,
         imgPath: 'img/zimbabwe.jpg',
-        description: 'Spent a month in Zimbabwe in 2019, quite an adventure!'
+        description: 'Spent a month in Zimbabwe in 2019, including 2 weeks traveling on foot, quite an adventure! Nyika Irikufamba ✌️'
+    },
+    {
+        name: 'Paraty',
+        lat: -23.22,
+        lon: -44.71,
+        imgPath: 'img/paraty.jpg',
+        description: "Historical town of Rio's Green Coast, with many nature and cultural attractions."
+    },
+    {
+        name: 'Pantanal',
+        lat: -17.7,
+        lon: -57.39,
+        imgPath: 'img/pantanal.jpg',
+        description: "The world's largest tropical wetland and largest flooded grasslands, home of unique biodiversity, with iconic animals like the jaguar and tuiuiú."
+    },
+    {
+        name: 'Lyon',
+        lat: 45.75,
+        lon: 4.84,
+        imgPath: 'img/lyon.jpg',
+        description: "This was my home in 2017-2019, when I did an academic exchange at Ecole Centrale de Lyon."
     },
 ]
